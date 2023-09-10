59718c174eed997ae59bff5bd6f69106832cd78d
Ji Hoon Byun <wlgns1588@naver.com>
2023-09-10 22:12:59 +0900

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -6,7 +6,7 @@ const Logo = (props) => (
     xmlns='http://www.w3.org/2000/svg'
     width='24'
     height='24'
-    viewBox='0 0 500 500'
+    viewBox='0 0 960 960'
     style={{
       opacity: 1
     }}
