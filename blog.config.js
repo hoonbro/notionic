851d5d60cdcec7b4902e87eb0e36771c3b45eb91
Ji Hoon Byun <wlgns1588@naver.com>
2023-09-10 21:47:16 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'wlgns1588@naver.com',
   link: 'https://github.com/hoonbro',
   newsletter: 'Notionic Weekly',
-  description: '주니어 개발자 성장 기록',
+  description: '주니어 개발자의 성장 기록',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Seoul', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
