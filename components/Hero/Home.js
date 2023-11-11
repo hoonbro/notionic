50bbcde5d9a25376113a5d6d1e6dd0595b582bd4
Ji Hoon Byun <wlgns1588@naver.com>
2023-11-11 22:17:39 +0900

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -28,7 +28,7 @@ const Hero = ({ blockMap }) => {
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
-        <div className='flex flex-col md:w-3/5 md:items-start mb-6 md:mb-0 text-left'>
+        <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 text-left'>
           <NotionRenderer
             className='md:ml-0'
             blockMap={blockMap}
@@ -36,48 +36,6 @@ const Hero = ({ blockMap }) => {
             subPageTitle={null}
           />
           <Social />
-          <div className='flex flex-col sm:flex-row sm:justify-center gap-4 mt-6'>
-            <Link passHref href='/contact' scroll={false}>
-              <button className='w-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'>
-                <MailIcon className='inline-block text-gray-600 dark:text-day h-7 w-7 mt-1' />
-                <span className='ml-4 flex items-start flex-col leading-none'>
-                  <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                    {t.HERO.HOME.CONTACT_BUTTON_DES}
-                  </span>
-                  <span className='font-medium'>{t.HERO.HOME.CONTACT_BUTTON}</span>
-                </span>
-              </button>
-            </Link>
-            {showCopied ? (
-              <button
-                disabled
-                className='bg-gray-200 dark:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'
-              >
-                <ClipboardCheckIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
-                <span className='ml-4 flex items-start flex-col leading-none'>
-                  <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                    {t.HERO.RSS_BUTTON_DES_COPIED}
-                  </span>
-                  <span className='font-medium'>
-                    {t.HERO.RSS_BUTTON_COPIED}
-                  </span>
-                </span>
-              </button>
-            ) : (
-              <button
-                onClick={() => clickCopy()}
-                className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'
-              >
-                <RssIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
-                <span className='ml-4 flex items-start flex-col leading-none'>
-                  <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                    {t.HERO.RSS_BUTTON_DES}
-                  </span>
-                  <span className='font-medium'>{t.HERO.HOME.RSS_BUTTON}</span>
-                </span>
-              </button>
-            )}
-          </div>
         </div>
         <div className='w-2/5'>
           <Avatar className='text-gray-600 dark:text-gray-300' />
